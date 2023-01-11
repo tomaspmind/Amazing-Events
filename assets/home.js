668adcaf8196cf5af91a004426d123151335c66e
tomaspmind <tomipp6@gmail.com>
2023-01-11 09:51:09 -0300

    primera pagina completada

---
+++ assets/home.js
@@ -0,0 +1,40 @@
+const { createApp } = Vue
+
+createApp( {
+	data(){
+		return {
+            
+			message : "hello",
+            amazingInfo : undefined,
+            amazingCard : undefined,
+            categorys : undefined,
+            valueOfSearch: "",
+            filterCard: "",
+            checked: [],
+		}
+	},
+    created(){
+        fetch("https://mindhub-xj03.onrender.com/api/amazing")
+        .then(response => response.json())
+        .then(data => {
+            this.amazingInfo = data
+            this.amazingCard = data.events
+            this.categorys = [...new Set(this.amazingCard.map(card => card.category))]
+            this.filterCard = [...this.amazingCard]
+        })
+        .catch(error => error.message)
+    },
+    methods:{
+        crossFilter: function(){
+            let filterPerSearch = this.amazingCard.filter(card => card.name.toLowerCase().startsWith(this.valueOfSearch.toLowerCase()))
+            if (this.checked.length === 0){
+                this.filterCard = filterPerSearch
+            }else{
+                let filterPerCheck = filterPerSearch.filter(card => this.checked.includes(card.category))
+                this.filterCard = filterPerCheck
+            }
+        }
+
+    },
+    
+}).mount("#app")
